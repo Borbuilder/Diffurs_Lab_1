1789f55add924d8834d2a7d0683b06f69a45005e
Borbuilder <nikitamaximfcn@yandex.ru>
2023-11-01 19:56:05 +0300

    Нашёл пример

--- main.py
+++ main.py
@@ -5,29 +5,37 @@ from matplotlib import pyplot as plt
 # функция, задающая правую часть
 # дифференциального уравнения y' = f(x, y)
 def firstRhs(x, y):
-    return (y ** 2 - y) / x
+    return y-x**2
 
 
 def secondRhs(x, y):
-    return cos(y - x) - 1
+    return (x+3-2*y)/2
 
+def thirdRhs(x, y):
+    return (((x**2) + (y**2))/2)-1
+
+def forthRhs(x, y):
+    return -x + sqrt(x**2 + 2*y)
+
+def fifthRhs(x, y):
+    return y/(x*(2*x*y + 1))
 
 # def yetAnotherExampleRhs(x, y):
 # return sin(y)
 
 def eulerStep(rhs, step, x0, y0):
-    return y0 + step * rhs(x0, y0)
+    return (y0 + step * rhs(x0, y0))
 
 
-N = 40  # количество узлов для сетки численного решения
-dx = 1  # шаг интегрирования по независимой переменной
-xPrev = 1
-yPrev = 0.1
+N = 100 # количество узлов для сетки численного решения
+dx = 0.1  # шаг интегрирования по независимой переменной
+xPrev = 0.01
+yPrev = -0.01
 xs = [xPrev]
 ys = [yPrev]
 
-# rhs = secondRhs
-rhs = secondRhs
+
+rhs = fifthRhs
 
 for i in range(N):
     xNext = xPrev + dx
